f8d1a0df939667d6c94015ce8f6d623498ef1e85
Dmitrii Shiriaev <dshiryaev@plesk.com>
2018-10-16 14:54:53 +0700

    FEATURE PT-5723 Content-type application/json for all responses

--- src/Sphring/MicroWebFramework/Controller/IndexController.php
+++ src/Sphring/MicroWebFramework/Controller/IndexController.php
@@ -26,25 +26,34 @@ class IndexController extends AbstractController
 
     public function action()
     {
+        $this->response->headers->add(['Content-Type' => 'application/json']);
 
         $version = $this->getRequest()->headers->get('X-Broker-API-Version') . '.0';
         try {
             $satisfy = $this->getBrokerVersionExpression()->satisfiedBy(new version($version));
         } catch (SemVerException $e) {
             $this->response->setStatusCode(Response::HTTP_PRECONDITION_FAILED);
-            return Response::$statusTexts[Response::HTTP_PRECONDITION_FAILED];
+            return $this->getErrorJson(
+                'PreconditionFailed',
+                Response::$statusTexts[Response::HTTP_PRECONDITION_FAILED]
+            );
         }
 
         if (empty($version) || !$satisfy) {
             $this->response->setStatusCode(Response::HTTP_PRECONDITION_FAILED);
-            return Response::$statusTexts[Response::HTTP_PRECONDITION_FAILED];
+            return $this->getErrorJson(
+                'PreconditionFailed',
+                Response::$statusTexts[Response::HTTP_PRECONDITION_FAILED]
+            );
         }
         $basicAuth = $this->getBasicAuth();
         $basicAuth->setRequest($this->request);
         if (!$basicAuth->auth()) {
             $this->response->setStatusCode(Response::HTTP_UNAUTHORIZED);
             $this->response->headers->add(['WWW-Authenticate' => 'Basic realm="' . HttpBasicAuthentifier::REALM . '"']);
-            return Response::$statusTexts[Response::HTTP_UNAUTHORIZED];
+            return $this->getErrorJson(
+                Response::$statusTexts[Response::HTTP_UNAUTHORIZED]
+            );
         }
 
         return null;
@@ -62,5 +71,29 @@ class IndexController extends AbstractController
         }
         $serviceBroker->setResponse($this->response);
         return $serviceBroker;
+    }
+
+    /**
+     * @param string $error A single word in camel case that uniquely identifies the error condition.
+     *  If present, MUST be a non-empty string.
+     *  Default: ''
+     * @param string $description A user-facing error message explaining why the request failed.
+     *  If present, MUST be a non-empty string.
+     *  Default: ''
+     *
+     * @return string JSON
+     *
+     * @see https://github.com/openservicebrokerapi/servicebroker/blob/v2.14/spec.md#service-broker-errors
+     */
+    public function getErrorJson($error = '', $description = '')
+    {
+        $result = [];
+        if ($error) {
+            $result['error'] = $error;
+        }
+        if ($description) {
+            $result['description'] = $description;
+        }
+        return json_encode($result, JSON_FORCE_OBJECT);
     }
 }
