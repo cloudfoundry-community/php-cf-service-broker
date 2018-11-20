9945e131d558888614dc8026bb33acd35d66a94e
Dmitrii Shiriaev <dshiryaev@plesk.com>
2018-11-20 15:21:07 +0700

    MINOR PT-5925 Add example of file stream logging. Switch default log level for stdout to "WARNING".

--- app/app.php
+++ app/app.php
@@ -6,7 +6,8 @@ require_once __DIR__ . '/../index.php';
 $logVars = ['_GET', '_POST', '_FILES', '_COOKIE', '_SESSION', '_SERVER'];
 
 $logger = new \Monolog\Logger("MicroWebFramework");
-$logger->pushHandler(new \Monolog\Handler\StreamHandler('php://stdout', \Monolog\Logger::INFO));
+$logger->pushHandler(new \Monolog\Handler\StreamHandler('php://stdout', \Monolog\Logger::WARNING));
+//$logger->pushHandler(new \Monolog\Handler\StreamHandler(__DIR__ . '/../cache/log.log', \Monolog\Logger::WARNING));
 
 $loggerSphring = \Arthurh\Sphring\Logger\LoggerSphring::getInstance();
 $loggerSphring->setLogger($logger);
